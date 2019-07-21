4b876ceb4e1018ccb25e9fbf7dc80c451d05d6fc
jain108shubhamtbt <42634889+jain108shubhamtbt@users.noreply.github.com>
2019-07-21 13:58:25 +0530

    Create print.go

---
+++ print.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("print message")
+}
+
+
+
+
+To Run this need to install go latest version and run command to ckeck go install successfuly or not.
+command to check version : go version
+command to run : go run print.go 
